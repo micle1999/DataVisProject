a5b6a615b292368f0313886e448699e2a303edce
Michal Šalaga <michalsalaga@MichalMacBook.local>
2023-11-16 10:51:49 +0100

    map with polygons , sliders, pie chart

--- app.R
+++ app.R
@@ -15,15 +15,24 @@ library(dplyr)
 library(leaflet)
 library(leaflet.extras)
 library(stringr)
+library(shinyWidgets)
+#library(gganimate)
 
 library(spData) # For getting spatial data
 library(sf) # For preserving spatial data
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+    theme = "styles.css",
     
+    setBackgroundImage(
+      src = "https://img.freepik.com/free-photo/peachy-blur-gradient-background-soft-vintage-style_53876-108717.jpg?w=2000&t=st=1699966170~exp=1699966770~hmac=5601c1c7ff8a723798c8c1823c3724703cb80eea5b176c9d698adf8461753bc9"
+    ),
+  
     # Application title
-    titlePanel("World Happiness Index"),
+    headerPanel(h1("World Happiness Index", align='center')),
+    
 
     # Sidebar with a slider input for number of bins 
     #sidebarLayout(
@@ -34,34 +43,30 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(type = "tabs",
-                  tabPanel("Map", leafletOutput("heatmap")),
-                  tabPanel("Plots", plotOutput("pie_chart"), 
-                           selectInput("input_year", "Select Year:",
-                              c(2006:2022)))
+                  tabPanel("Map", sliderInput("slider_year", "Select year:",
+                                              min = 2006, max = 2022, value = 2014, step=1, sep=" ", ticks=FALSE, width = "100%"),
+                  leafletOutput("map_graph",  width = "1000px", height = "600px")),
+                  
+                  tabPanel("Plots", selectInput("input_year", "Select Year:", c(2006:2022)), plotOutput("pie_chart",  width = "1000px", height = "600px")),
+                  
+                  # add more panels here
+                 )
+          ,class="flex-center"
       )
-      
-      #leafletOutput(outputId = "heatmap"),
-      #fluidRow(
-        #splitLayout(cellWidths = c("50%", "50%"), plotOutput("line_plot"), plotOutput("line_plot2"))
-      #)
-    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
+    
+    print(world[c(2,11)]$name_long)
+    
     #load dataset
-    data <- read.csv(file = 'WHI_geo.csv',header = TRUE, sep = ';', colClasses = c('character','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+    data <- read.csv(file = 'WHI_geo_new.csv',header = TRUE, sep = ';', colClasses = c('character','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
     
     #Categorize Life Ladder
     data$happiness_cat <- ifelse(data$Life.Ladder <= 4.5, "Unhappy", ifelse(data$Life.Ladder <= 6 & data$Life.Ladder >4.5, "Neutral", ifelse(data$Life.Ladder > 6, "Happy", "Other")))
       
-    #define the color of for the depth of the earquakes
-    color_pal <- colorFactor(
-      palette = c('green', 'yellow', 'red'),
-      domain = data$happiness_cat
-    )
-    
+ 
     #Create Pie chart (Question no.2a)
     output$pie_chart <- renderPlot({
       
@@ -77,45 +82,44 @@ server <- function(input, output) {
       ggplot(pie_count, aes(x = 2, y = count, fill = cat)) +
         geom_bar(stat = "identity", color = "white") +
         coord_polar(theta = "y", start = 0)+
-        geom_text(aes(y = label_pos, label = count), color = "white")+
+        geom_text(aes(y = label_pos, label = count), color = "black")+
         theme_void()+
-        xlim(0.5, 2.5)
+        xlim(0.5, 2.5)+
+        scale_fill_manual(values = c("#98FB98", "#FFFF99", "#FF9999"))
     })
     
     
+    
+    
     #create the map
-    output$heatmap <- renderLeaflet({
+    output$map_graph <- renderLeaflet({
+      
+      #selected data
+      selected_data <- data[data$year == input$slider_year, ]
       
       #spatial data from spData library
       mapData <- world[c(2,11)]
       
-      print(mapData)
-      
       #join data with spatial data
-      countries <- left_join(data, mapData, c("Country.name" = "name_long"))
+      countries <- left_join(selected_data, mapData, c("Country.name" = "name_long"))
       
       #Color pallete for the map  
-      pal <- colorNumeric(palette = "YlOrRd", domain = countries$Life.Ladder)  
+      pal <- colorNumeric(palette = c("#FF9999", "#FFFF99", "#98FB98"), domain = countries$Life.Ladder)  
       
       map <- leaflet(data) %>% 
-        setView(lng = -99, lat = 45, zoom = 2)  %>% #setting the view over ~ center of North America
+        setView(lng = 0, lat = 45, zoom = 2)  %>% #setting the view over ~ center of North America
         addTiles()
-        #addCircles(opacity = 70, data = data, lat = data$Latitude, lng = data$Longitude, weight = 1, radius = 100000, popup = ~as.character(Country.name), label = ~as.character(paste0("Happines of the country: ", sep = " ", happiness_cat)), color = ~color_pal(happiness_cat), fillOpacity = 0.5)
-      
+    
       #add polygons to map
       map %>% addPolygons(data = countries$geom,
                           fillColor = pal(countries$Life.Ladder),
                           fillOpacity = .7,
                           color = "grey",
-                          weight = 1)
+                          weight = 1,
+                          popup = paste("Country: ", countries$Country.name, "<br>",
+                                        "Happiness Index: ", countries$Life.Ladder, "<br>"))
     })
     
-    
-    output$line_plot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        ggplot(data %>% filter(Country.name == 'Slovakia'), 
-             aes(year, `Life.Ladder`,  group = Country.name)) + geom_line()
-    })
    
 }
 
